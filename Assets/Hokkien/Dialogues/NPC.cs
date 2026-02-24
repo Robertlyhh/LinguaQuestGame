a5a44104931a1b2923f011c8c54730c5baf563d5
percy-ai <startdash71@gmail.com>
2026-02-23 22:48:53 -0500

    Change dialogue UI and enable Interaction

--- Assets/Hokkien/Dialogues/NPC.cs
+++ Assets/Hokkien/Dialogues/NPC.cs
@@ -38,6 +38,7 @@ public class NPC : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+        Debug.Log("Interaction triggered");
         if (isDialogueActive)
         {
             NextLine();
