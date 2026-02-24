3548c6f2b878781c3256ba00d74d94ed3d3500de
percy-ai <startdash71@gmail.com>
2026-02-23 23:07:18 -0500

    rm log call

--- Assets/Hokkien/Dialogues/NPC.cs
+++ Assets/Hokkien/Dialogues/NPC.cs
@@ -38,7 +38,6 @@ public class NPC : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        Debug.Log("Interaction triggered");
         if (isDialogueActive)
         {
             NextLine();
